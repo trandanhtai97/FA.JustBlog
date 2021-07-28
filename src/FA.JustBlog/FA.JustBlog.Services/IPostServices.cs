4c4365de3a09eeab4db1bcefff1a3f0b43902085
DanhTai <danhtai97tb@gmail.com>
2021-07-28 21:36:48 +0700

    Create MVC

---
+++ src/FA.JustBlog/FA.JustBlog.Services/IPostServices.cs
@@ -0,0 +1,14 @@
+﻿using FA.JustBlog.Models.Common;
+using FA.JustBlog.Services.BaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA.JustBlog.Services
+{
+    public interface IPostServices : IBaseService<Post>
+    {
+    }
+}
---
+++ src/FA.JustBlog/FA.JustBlog.Services/PostServices.cs
@@ -0,0 +1,18 @@
+﻿using FA.JustBlog.Data.Infrastructure;
+using FA.JustBlog.Models.Common;
+using FA.JustBlog.Services.BaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA.JustBlog.Services
+{
+    public class PostServices : BaseServices<Post>, IPostServices
+    {
+        public PostServices(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
