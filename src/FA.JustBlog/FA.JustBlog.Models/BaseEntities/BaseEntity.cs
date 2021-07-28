565a9f7eea107ad977c391788b378760a6ac7026
DanhTai <danhtai97tb@gmail.com>
2021-07-28 10:43:24 +0700

    Create data

---
+++ src/FA.JustBlog/FA.JustBlog.Models/BaseEntities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FA.JustBlog.Models.BaseEntities
+{
+    public class BaseEntity : IBaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime InsertedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
